f0f1f4837bb3ab45f63520f6213bbbb6f309da3e
PM-lopez <pablo.montoya.l@usach.cl>
2024-11-12 16:30:53 -0300

    Update and rename propietario yo to propietario yo.py

--- Idk/propietario yo.py
+++ Idk/propietario yo.py
@@ -1,12 +1,15 @@
 import numpy as np
 from collections import Counter
 import matplotlib.pyplot as plt
+from Tarea import promedio,mediana,rango_intercuartil,mad,desviacion_estandar,moda
 
 archivo=open("grupo_local.csv","r")
 archivo.readline()
 tipos_galaxias=[]
+distancias=[]
 for lin in archivo:
     tipos_galaxias.append(lin.split(",")[5])
+    distancias.append(float(lin.split(",")[3]))
 archivo.close()
 
 cc=Counter(tipos_galaxias)
@@ -22,3 +25,10 @@ ax1.set_xlabel("Tipo de galaxia",fontsize=13)
 ax1.set_ylabel("N",fontsize=13)
 ax1.set_title("Tipos de galaxias en el Grupo local",fontsize=13)
 plt.show()
+
+print("Distancia promedio: ", promedio(distancia))
+print("Distancia mediana: ", mediana(distancia))
+print("Distancia IQR: ", rango_intercuartil(distancia))
+print("Distancia MAD: ", mad(distancia))
+print("Distancia desviacion_estandar: ", desviacion_estandar(distancia))
+print("Moda de tipo de galaxias: ", moda(tipos_galaxias))
